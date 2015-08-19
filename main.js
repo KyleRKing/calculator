fcaeb6b7bcd1bfe05621aad9d3ecfe5b8256e505
Kyle King <kylerking@me.com>
2015-08-18 20:15:00 -0700

    tonights final files to date

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-var numbers = document.querySelectorAll('.input');
-	console.log(numbers);
+var varString = ["click"]
+var leslie = document.querySelectorAll(".buttons");
+
+
+for (var i =0 ; i < leslie.length; i++ ) { //cycle through everything in the leslie array
+    leslie[i].addEventListener("click", //attach an event listener to each item
+        function (){
+            console.log(this.innerHTML); // console.log that item's inner HTML when clcked
+
+        })
+}
+
+
 
 
 
